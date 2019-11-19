5559366bcb9993ede71f0674a98fd2c8cdf85bdc
Coal Cooper <cocowmn@gmail.com>
2019-11-19 02:35:33 -0500

    create wizard interface for generating new wacky fill-ins

---
+++ wizard.js
@@ -0,0 +1,65 @@
+const wordbank = new Wordbank( '#variables' )
+
+const story = document.querySelector( 'form[name=story]' )
+const textInput = document.querySelector( 'textarea[name=content]' )
+const refreshVariablesButton = document.querySelector( 'input[type=submit][name=content-submit]' )
+const submitFormButton = document.querySelector( 'button[type=submit][name=wordbank-submit]' )
+
+const updateSubmitFormButtonDisabledStatus = () => {
+  submitFormButton.disabled = !wordbank.isValid()
+  if( submitFormButton.disabled ) {
+    submitFormButton.classList.remove( 'success' )
+    submitFormButton.classList.remove( 'white-text' )
+
+    submitFormButton.classList.add( 'gray' )
+    submitFormButton.classList.add( 'seethrough' )
+  } else {
+    submitFormButton.classList.remove( 'gray' )
+    submitFormButton.classList.remove( 'seethrough' )
+    
+    submitFormButton.classList.add( 'success' )
+    submitFormButton.classList.add( 'white-text' )
+  }
+}
+
+textInput.addEventListener(
+  'input',
+  ( event ) => { refreshVariablesButton.click() }
+)
+
+story.addEventListener(
+  'submit',
+  ( event ) => {
+    event.preventDefault()
+
+    const userInput = document.querySelector( '[name=content]' ).value
+    const variablePattern = /\{\s*([a-z0-9\_\-\$]+)\s*\}/gmi
+    const extractedVariables = ( userInput.match( variablePattern ) || [ ])
+      .map( match => match.replace( /(?:\{|\})/g, '' ).replace( /\s+/, '' ) )
+
+    wordbank.accept( extractedVariables )
+    updateSubmitFormButtonDisabledStatus()
+    submitFormButton.disabled = !wordbank.isValid()
+  })
+
+submitFormButton.addEventListener(
+  'click',
+  ( event ) => {
+    event.preventDefault()
+
+    if( wordbank.getData().length > 0 ) {
+      const title = document.querySelector( '[name=title]' ).value
+
+      console.log( 'we did it! The world is your oyster. Do something with this data!', {
+        title, 
+        data: wordbank.getData(), 
+        document: textInput.value
+      })
+    }
+  })
+
+document.addEventListener(
+  'click',
+  () => {
+    updateSubmitFormButtonDisabledStatus()
+  })
